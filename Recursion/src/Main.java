8ecf95dec480ecd4bd036724252c5fb6acf53a2d
NIKITHA <NIKITHA@Admin-PC>
2022-06-25 15:58:43 +0530

    Recursion

---
+++ Recursion/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void print_digits(int n) {
+		if (n<=9) {
+			System.out.println(n);
+		return;
+		}
+	print_digits(n/10);
+	System.out.println(n%10);
+}
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+print_digits(n);
+
+
+	}
+
+}
